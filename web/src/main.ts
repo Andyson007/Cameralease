02f399c533f672b67eefd4d4bca05f1801e44b3b
averieyy <58344917+averieyy@users.noreply.github.com>
2024-01-17 11:03:13 +0100

    card with availablility information

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,1 +1,32 @@
-fetch("/api/ping", {method: "POST"});
+const cameralist = document.getElementById("cameralist");
+if (!cameralist) console.error("cameralist does not exist");
+
+function createCard (name: string, uid: string) {
+  if (!cameralist) return;
+  let base = document.createElement("div");
+  base.classList.add("cameracard", "available");
+  base.setAttribute("uid", uid);
+
+  let left = document.createElement("div");
+  left.classList.add("camcardleft");
+
+  let right = document.createElement("div");
+  right.classList.add("camcardright")
+
+  let nameplate = document.createElement("span");
+  nameplate.classList.add("nameplate");
+  nameplate.innerText = name;
+
+  let idtext = document.createElement("span");
+  idtext.classList.add("idtext");
+  idtext.innerText = uid;
+
+  right.appendChild(nameplate);
+  right.appendChild(idtext);
+  
+  base.appendChild(left);
+  base.appendChild(right);
+  cameralist.appendChild(base);
+}
+
+createCard("camera model or something", "CSX 8888");
