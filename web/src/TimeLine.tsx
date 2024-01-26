ddb2068c6713dbfa8bdbc02ce3bd1e9eb31d4f85
averieyy <58344917+averieyy@users.noreply.github.com>
2024-01-26 22:12:39 +0100

    Allowed undefined progress

--- web/src/TimeLine.tsx
+++ web/src/TimeLine.tsx
@@ -1,7 +1,7 @@
 import TimelineSpan from "./TimelineSpan";
 import "./TimeLine.scss";
 
-export default function TimeLine({timeSpan, progress, textVis, timeLineSpans}: {timeSpan: number[], progress: number, textVis: boolean, timeLineSpans: number[][]}) {
+export default function TimeLine({timeSpan, progress, textVis, timeLineSpans}: {timeSpan: number[], progress: number | undefined, textVis: boolean, timeLineSpans: number[][]}) {
   
   const dateTimeSpan = timeSpan.map(f => new Date(f*1000)); // Make the timespan into date objects to avoid repetition
   const textTimeSpan = dateTimeSpan.map(f => f.toLocaleTimeString("no-NB", {timeStyle: "short"})); // turn it into text (military time)
@@ -12,9 +12,11 @@ export default function TimeLine({timeSpan, progress, textVis, timeLineSpans}: {
         {timeLineSpans.map(tls => 
           <TimelineSpan daystart={timeSpan[0]} dayend={timeSpan[1]} label="username" length={tls[1]} start={tls[0]}></TimelineSpan>
         )}
-        <div style={
-          {left: `${progress}%`, display: (progress >= 0 && progress <= 100 ? "" : "none")} // Current time in day in percent
-        } className="currenttime"></div>
+        { progress &&
+          <div style={
+            {left: `${progress}%`, display: (progress >= 0 && progress <= 100 ? "" : "none")} // Current time in day in percent
+          } className="currenttime"></div>
+        }
       </div>
       {textVis &&
         <div className="timelabels">
